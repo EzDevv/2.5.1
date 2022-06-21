9dd753c10ab18293755fb883e771da94aafec365
Evgeniy Antonov <zhekahead@gmail.com>
2022-06-21 09:38:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let name = "Ilya";
+
+alert( `hello ${1}` ); //Выведет алерт "hello 1"
+alert( `hello ${"name"}`); //Выведет алерт "hello name"
+alert( `hello ${name}`); //Выведет алерт "hello Ilya"
